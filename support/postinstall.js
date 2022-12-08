cf22c43d2fc4267e92631f39e41950fe2f0f0979
nvitas <nvitas@gmail.com>
2022-12-08 16:44:17 -0500

    added post install instructions

---
+++ support/postinstall.js
@@ -0,0 +1,22 @@
+const banner = `
+
+         888                      888       d8b                   
+         888                      888       Y8P                   
+         888                      888                             
+.d8888b  888888  8888b.   .d8888b 888  888 8888  .d88b.  888  888 
+88K      888        "88b d88P"    888 .88P "888 d88""88b 888  888 
+"Y8888b. 888    .d888888 888      888888K   888 888  888 888  888 
+     X88 Y88b.  888  888 Y88b.    888 "88b  888 Y88..88P Y88b 888 
+ 88888P'  "Y888 "Y888888  "Y8888P 888  888  888  "Y88P"   "Y88888 
+                                            888               888 
+                                           d88P          Y8b d88P 
+                                         888P"            "Y88P"  
+`;
+const message = `
+             type 'sj' in your terminal to start
+             
+             
+`;
+
+console.log(banner);
+console.log(message);
